8449bd448a59d7b1191e1ca5bbe540e68e31744a
7-18 <kevinbrianbr@gmail.com>
2023-09-21 16:23:47 -0500

    add: Update clasification model

--- models/clasification.js
+++ models/clasification.js
@@ -1,8 +1,12 @@
 import mongoose from "mongoose";
 
 const clasificationSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  register_at: { type: Date, default: Date.now },
+  name: { type: String, minLength: 2, required: true },
+  register_at: { type: Date, immutable: true, default: Date.now },
+  update_at: { type: Date, default: Date.now },
 });
 
-export const Clasification = mongoose.model("clasification", clasificationSchema);
+export const Clasification = mongoose.model(
+  "clasification",
+  clasificationSchema
+);
