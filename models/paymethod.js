3200df5f22bce87819c6d8c94f622ee3be7c73b4
7-18 <kevinbrianbr@gmail.com>
2023-09-21 16:24:34 -0500

    add: Update pay method model

--- models/paymethod.js
+++ models/paymethod.js
@@ -1,8 +1,9 @@
 import mongoose from "mongoose";
 
 const paymethodSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  register_at: { type: Date, default: Date.now },
+  name: { type: String, minLength: 4, required: true },
+  register_at: { type: Date, immutable: true, default: Date.now() },
+  update_at: { type: Date, default: Date.now() },
 });
 
 export const Paymethod = mongoose.model("paymethod", paymethodSchema);
