7faa563fab77cebbe252cb619d1d4b0df678e766
7-18 <kevinbrianbr@gmail.com>
2023-09-21 17:41:36 -0500

    add: clasification routes

--- routes/clasification.js
+++ routes/clasification.js
@@ -4,5 +4,9 @@ import Clasification from "../controllers/clasification.js";
 const router = express.Router();
 
 router.post("/create", Clasification.createClasification);
+router.get("/", Clasification.getClasifications);
+router.get("/:id", Clasification.getClasificationById);
+router.put("/update/:id", Clasification.updateClasification);
+router.delete("/delete/:id", Clasification.deleteClasification);
 
 export default router;
