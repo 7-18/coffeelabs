f33d8f8689cf8d21e82088978ba69a8af3c73e7f
7-18 <kevinbrianbr@gmail.com>
2023-09-21 15:15:55 -0500

    add: Update quantity default value

--- models/sale.js
+++ models/sale.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const saleSchema = new mongoose.Schema({
   product_id: { type: mongoose.Schema.Types.ObjectId, ref: "product", required: true },
   paymethod_id: { type: mongoose.Schema.Types.ObjectId, ref: "paymethod", required: true },
-  quantity: { type: Number, required: true },
+  quantity: { type: Number, default: 1, required: true },
   sale_at: { type: Date, default: Date.now },
 });
 
