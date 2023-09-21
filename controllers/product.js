3bbd1d1e1e22666df270be557b3fa29409a6df57
7-18 <kevinbrianbr@gmail.com>
2023-09-21 18:15:11 -0500

    add: product functions - create, update, delete, get

---
+++ controllers/product.js
@@ -0,0 +1,86 @@
+import { Product } from "../models/product.js";
+
+const createProduct = async (req, res) => {
+  if (
+    !req.body.name ||
+    !req.body.description ||
+    !req.body.stock ||
+    !req.body.price ||
+    !req.body.clasification_id
+  ) {
+    return res.status(400).send({ message: "Content can not be empty!" });
+  }
+
+  const product = new Product({
+    name: req.body.name,
+    description: req.body.description,
+    stock: req.body.stock,
+    price: req.body.price,
+    clasification_id: req.body.clasification_id,
+  });
+
+  try {
+    const savedProduct = await product.save();
+    return res.status(201).send(savedProduct);
+  } catch (err) {
+    return res.status(400).send(err);
+  }
+};
+
+const getProducts = async (req, res) => {
+  try {
+    const products = await Product.find();
+    return res.status(200).send(products);
+  } catch (err) {
+    return res.status(500).send({ message: err.message });
+  }
+};
+
+const getProductById = async (req, res) => {
+  try {
+    const product = await Product.findOne({ _id: req.params.id });
+    return res.status(200).send(product);
+  } catch (err) {
+    return res.status(500).send({ message: err.message });
+  }
+};
+
+const updateProduct = async (req, res) => {
+  try {
+    const product = await Product.findOneAndUpdate(
+      { _id: req.params.id },
+      {
+        name: req.body.name,
+        description: req.body.description,
+        stock: req.body.stock,
+        price: req.body.price,
+        clasification_id: req.body.clasification_id,
+        update_at: Date.now(),
+      },
+      {
+        new: true,
+        setDefaultsOnInsert: true,
+      }
+    );
+    return res.status(202).send(product);
+  } catch (err) {
+    return res.status(500).send({ message: err.message });
+  }
+};
+
+const deleteProduct = async (req, res) => {
+  try {
+    await Product.findOneAndDelete({ _id: req.params.id });
+    return res.status(200).send({ message: "Product deleted successfully." });
+  } catch (err) {
+    return res.status(500).send({ message: err.message });
+  }
+};
+
+export default {
+  createProduct,
+  getProducts,
+  getProductById,
+  updateProduct,
+  deleteProduct,
+};
