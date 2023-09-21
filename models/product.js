000a8a8ad8b3341eec844b5f6ce16a54712815bd
7-18 <kevinbrianbr@gmail.com>
2023-09-21 15:15:35 -0500

    add: Update stock and price default value

--- models/product.js
+++ models/product.js
@@ -3,8 +3,8 @@ import mongoose from "mongoose";
 const productSchema = new mongoose.Schema({
   name: { type: String, required: true },
   description: { type: String, required: true },
-  stock: { type: Number, required: true },
-  price: { type: Number, required: true },
+  stock: { type: Number, default: 0, required: true },
+  price: { type: Number, default: 0, required: true },
   clasification_id: { type: mongoose.Schema.Types.ObjectId, ref: "clasification", required: true },
   register_at: { type: Date, default: Date.now },
 });
