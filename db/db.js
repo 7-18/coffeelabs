3631a43428a9ea5659aff4bce57e0284d7584fc2
7-18 <kevinbrianbr@gmail.com>
2023-09-21 14:20:17 -0500

    chore: Configure DB connection

---
+++ db/db.js
@@ -0,0 +1,24 @@
+import { MongoClient, ServerApiVersion } from "mongodb";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const client = new MongoClient(process.env.DB_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+export async function dbConnection() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    await client.close();
+  }
+}
