0d65485594794febed4f262bfc1fa9c044c52a97
7-18 <kevinbrianbr@gmail.com>
2023-09-21 17:41:19 -0500

    add: clasification functions - create, get, update, delete

--- controllers/clasification.js
+++ controllers/clasification.js
@@ -2,8 +2,7 @@ import { Clasification } from "../models/clasification.js";
 
 const createClasification = async (req, res) => {
   if (!req.body.name) {
-    res.status(400).send({ message: "Content can't be empty!" });
-    return;
+    return res.status(400).send({ message: "Content can't be empty!" });
   }
 
   const clasification = new Clasification({
@@ -12,10 +11,67 @@ const createClasification = async (req, res) => {
 
   try {
     const savedClasification = await clasification.save();
-    res.status(201).send(savedClasification);
+    return res.status(201).send(savedClasification);
   } catch (err) {
-    res.status(400).send({ message: err.message });
+    return res.status(400).send({ message: err.message });
   }
 };
 
-export default { createClasification };
+const getClasifications = async (req, res) => {
+  try {
+    const clasifications = await Clasification.find();
+    if (clasifications.length === 0) {
+      return res.status(404).send({ message: "No clasifications found." });
+    }
+    return res.send(clasifications);
+  } catch (err) {
+    return res.status(500).send({ message: err.message });
+  }
+};
+
+const getClasificationById = async (req, res) => {
+  try {
+    const clasification = await Clasification.findOne({ _id: req.params.id });
+    return res.send(clasification);
+  } catch (err) {
+    return res.status(500).send({ message: err.message });
+  }
+};
+
+const updateClasification = async (req, res) => {
+  try {
+    const clasification = await Clasification.findOneAndUpdate(
+      { _id: req.params.id },
+      {
+        name: req.body.name,
+        update_at: Date.now(),
+      },
+      {
+        new: true,
+        setDefaultsOnInsert: true,
+      }
+    );
+    res.send({ message: "Clasification updated successfully." }, clasification);
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
+};
+
+const deleteClasification = async (req, res) => {
+  try {
+    const clasification = await Clasification.findOneAndDelete({
+      _id: req.params.id,
+    });
+    res.send({ message: "Clasification deleted successfully." });
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
+};
+
+export default {
+  createClasification,
+  getClasifications,
+  getClasificationById,
+  updateClasification,
+  deleteClasification,
+};
