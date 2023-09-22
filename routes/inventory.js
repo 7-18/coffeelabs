1929a27ef8ea616d3ddada18cc254f695a085c72
7-18 <kevinbrianbr@gmail.com>
2023-09-22 14:25:36 -0500

    chore: move files to src

--- routes/inventory.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import * as Inventory from "../controllers/inventory.js";
-
-const router = express.Router();
-
-router.get("/total-stock", Inventory.getTotalStock);
-router.get("/total-sales", Inventory.getTotalSales);
-
-export default router;
