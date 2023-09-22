e036e273483853aeae1af3a78cc2c6590036264e
7-18 <kevinbrianbr@gmail.com>
2023-09-21 19:28:37 -0500

    add: new product routes

--- routes/product.js
+++ routes/product.js
@@ -4,5 +4,9 @@ import Product from "../controllers/product.js";
 const router = express.Router();
 
 router.post("/create", Product.createProduct);
+router.get("/", Product.getProducts);
+router.get("/:id", Product.getProductById);
+router.put("/update/:id", Product.updateProduct);
+router.delete("/delete/:id", Product.deleteProduct);
 
 export default router;
