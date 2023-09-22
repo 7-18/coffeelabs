a039ab6f71e86a455f7e067b64de1b39bbbc405d
7-18 <kevinbrianbr@gmail.com>
2023-09-22 12:14:05 -0500

    chore: move files to src

---
+++ src/v1/routes/sale.js
@@ -0,0 +1,10 @@
+import express from "express";
+import * as Sales from "../../controllers/sale.js";
+
+const router = express.Router();
+
+router.post("/create", Sales.createSale);
+router.get("/", Sales.getSales);
+router.get("/:id", Sales.getSaleById);
+
+export default router;
