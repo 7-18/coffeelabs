c3e523e38669b7c96a63fb58d31b239d92a315c6
7-18 <kevinbrianbr@gmail.com>
2023-09-22 14:27:42 -0500

    feat: add OpenAPI doc to Inventory

--- src/v1/routes/inventory.js
+++ src/v1/routes/inventory.js
@@ -3,6 +3,64 @@ import * as Inventory from "../../controllers/inventory.js";
 
 const router = express.Router();
 
+/**
+ * @openapi
+ * tags:
+ *   name: Inventory
+ */
+
+/**
+ * @openapi
+ * /api/v1/inventory/total-stock:
+ *   get:
+ *     summary: Get total stock available
+ *     tags: [Inventory]
+ *     responses:
+ *       200:
+ *         description: OK
+ *         content:
+ *          application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               totalStock:
+ *                 type: integer
+ *                 example: 100
+ *         500:
+ *          description: FAILED
+ *          content:
+ *           application/json:
+ *            schema:
+ *             type: object
+ *             properties:
+ *               message:
+ *                 type: string
+ * /api/v1/inventory/total-sales:
+ *   get:
+ *     summary: Get total sales
+ *     tags: [Inventory]
+ *     responses:
+ *       200:
+ *         description: OK
+ *         content:
+ *          application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               totalSales:
+ *                 type: integer
+ *                 example: 100
+ *         500:
+ *          description: FAILED
+ *          content:
+ *           application/json:
+ *            schema:
+ *             type: object
+ *             properties:
+ *               message:
+ *                 type: string
+*/
+
 router.get("/total-stock", Inventory.getTotalStock);
 router.get("/total-sales", Inventory.getTotalSales);
 
