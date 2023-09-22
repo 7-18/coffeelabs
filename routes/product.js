9953eba4205511e62d51c23dc7df8124f4a88249
7-18 <kevinbrianbr@gmail.com>
2023-09-21 20:16:48 -0500

    feat: add filter by clasification route

--- routes/product.js
+++ routes/product.js
@@ -1,11 +1,12 @@
 import express from "express";
-import Product from "../controllers/product.js";
+import * as Product from "../controllers/product.js";
 
 const router = express.Router();
 
 router.post("/create", Product.createProduct);
 router.get("/", Product.getProducts);
 router.get("/:id", Product.getProductById);
+router.get("/clasification/:id", Product.getProductByClasification);
 router.put("/update/:id", Product.updateProduct);
 router.delete("/delete/:id", Product.deleteProduct);
 
