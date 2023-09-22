ba25b1ba7d106bf5e55dcb495a33528973e1fc0d
7-18 <kevinbrianbr@gmail.com>
2023-09-22 14:26:29 -0500

    feat: add OpenAPI doc to Clasification

--- src/models/clasification.js
+++ src/models/clasification.js
@@ -1,5 +1,33 @@
 import mongoose from "mongoose";
 
+/**
+ * @openapi
+ * components:
+ *  schemas:
+ *    Clasification:
+ *      type: object
+ *      properties:
+ *        _id:
+ *          type: string
+ *          example: 650d055db8b6303337f3906a
+ *        name:
+ *          type: string
+ *          minLength: 2
+ *          required: true
+ *          example: Ground Coffee
+ *        register_at:
+ *          type: date
+ *          immutable: true
+ *          example: 2023-09-22T03:09:17.838+00:00
+ *        update_at:
+ *          type: date
+ *          example: 2023-09-22T13:45:02.838+00:00
+ *      example:
+ *        _id: 650d055db8b6303337f3906a
+ *        name: Ground Coffee
+ *        register_at: 2023-09-22T03:09:17.838+00:00
+ *        update_at: 2023-09-22T13:45:02.838+00:00
+ */
 const clasificationSchema = new mongoose.Schema({
   name: { type: String, minLength: 2, required: true },
   register_at: { type: Date, immutable: true, default: Date.now },
--- src/v1/routes/clasification.js
+++ src/v1/routes/clasification.js
@@ -3,6 +3,158 @@ import * as Clasification from "../../controllers/clasification.js";
 
 const router = express.Router();
 
+/**
+ * @openapi
+ * tags:
+ *   name: Clasification
+ */
+
+/**
+ * @openapi
+ * /api/v1/clasifications:
+ *   get:
+ *     summary: Get all clasifications
+ *     tags: [Clasification]
+ *     parameters:
+ *       - in: query
+ *         name: p
+ *         schema:
+ *           type: integer
+ *         default: 1
+ *         description: Page
+ *       - in: query
+ *         name: limit
+ *         schema:
+ *           type: integer
+ *         default: 10
+ *         description: Limit
+ *     responses:
+ *       200:
+ *         description: OK
+ *         content:
+ *          application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               clasifications:
+ *                 type: array
+ *                 items:
+ *                   $ref: '#/components/schemas/Clasification'
+ *                 page:
+ *                   type: integer
+ *                 totalPages:
+ *                   type: integer
+ *       500:
+ *         description: FAILED
+ *         content:
+ *          application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               message:
+ *                 type: string
+ * /api/v1/clasifications/create:
+ *   post:
+ *     summary: Create a clasification
+ *     tags: [Clasification]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 minLength: 2
+ *                 required: true
+ *                 example: Black Coffee
+ *     required:
+ *       - name
+ *     example:
+ *       name: Black Coffee
+ *     responses:
+ *       201:
+ *         description: CREATED
+ *         content:
+ *          application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Clasification'
+ *       500:
+ *         description: FAILED
+ *         content:
+ *          application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               message:
+ *                 type: string
+ * /api/v1/clasifications/update/{id}:
+ *   put:
+ *     summary: Update a clasification
+ *     tags: [Clasification]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 minLength: 2
+ *                 required: true
+ *                 example: Black Coffee
+ *     required:
+ *       - name
+ *     example:
+ *       name: Black Coffee
+ *     responses:
+ *       202:
+ *         description: ACCEPTED
+ *         content:
+ *          application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Clasification'
+ *       500:
+ *         description: FAILED
+ *         content:
+ *          application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               message:
+ *                 type: string
+ * /api/v1/clasifications/delete/{id}:
+ *   delete:
+ *     summary: Delete a clasification
+ *     tags: [Clasification]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *     responses:
+ *       200:
+ *         description: DELETED
+ *       500:
+ *         description: FAILED
+ *         content:
+ *          application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               message:
+ *                 type: string
+ */
+
 router
   .get("/", Clasification.getClasifications)
   .post("/create", Clasification.createClasification)
