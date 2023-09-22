4d4ccd55e45dea6eee78df3975f12e707d413780
7-18 <kevinbrianbr@gmail.com>
2023-09-22 12:14:47 -0500

    chore: move file to src

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { dbConnection } from "./db/db.js";
+import Product_v1 from "./v1/routes/product.js";
+import Paymethod_v1 from "./v1/routes/paymethod.js";
+import Clasification_v1 from "./v1/routes/clasification.js";
+import Sales_v1 from "./v1/routes/sale.js";
+import Inventory_v1 from "./v1/routes/inventory.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT ?? 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/v1/products", Product_v1);
+app.use("/api/v1/clasifications", Clasification_v1);
+app.use("/api/v1/paymethods", Paymethod_v1);
+app.use("/api/v1/sales", Sales_v1);
+app.use("/api/v1/inventory", Inventory_v1);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+dbConnection();
