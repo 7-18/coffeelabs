05a6da7ba7c63d0dd4cb55943ed11ce4763a569e
7-18 <kevinbrianbr@gmail.com>
2023-09-21 20:15:06 -0500

    feat: add enpoint of inventory

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ import cors from "cors";
 import dotenv from "dotenv";
 import { dbConnection } from "./db/db.js";
 import Product from "./routes/product.js";
+import Paymethod from "./routes/paymethod.js";
 import Clasification from "./routes/clasification.js";
+import Sales from "./routes/sale.js";
+import Inventory from "./routes/inventory.js";
 
 dotenv.config();
 
@@ -13,8 +16,11 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/product", Product);
-app.use("/api/clasification", Clasification);
+app.use("/api/products", Product);
+app.use("/api/clasifications", Clasification);
+app.use("/api/paymethods", Paymethod);
+app.use("/api/sales", Sales);
+app.use("/api/inventory", Inventory);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
