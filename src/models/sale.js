74ee3f1965d64f159fff861e9eaaf3e9f52cf6e6
7-18 <kevinbrianbr@gmail.com>
2023-09-22 14:27:21 -0500

    feat: add OpenAPI doc to Sale

--- src/models/sale.js
+++ src/models/sale.js
@@ -1,5 +1,37 @@
 import mongoose from "mongoose";
 
+/**
+ * @openapi
+ * components:
+ *  schemas:
+ *    Sale:
+ *      type: object
+ *      properties:
+ *        _id:
+ *          type: string
+ *          example: 650d0895ecc24b20358b4e99
+ *        product_id:
+ *          type: string
+ *          ref: "products"
+ *        paymethod_id:
+ *          type: string
+ *          ref: "paymethods"
+ *        quantity:
+ *          type: number
+ *          default: 1
+ *          min: 1
+ *          required: true
+ *        sale_at:
+ *          type: date
+ *          immutable: true
+ *      example:
+ *        _id: 650d0895ecc24b20358b4e99
+ *        product_id: 650d0895ecc24b20358b4e99
+ *        paymethod_id: 650cca29cbaa1ff892f26456
+ *        quantity: 2
+ *        sale_at: 2023-09-22T03:09:17.838+00:00
+ */
+
 const saleSchema = new mongoose.Schema({
   product_id: { type: mongoose.Schema.Types.ObjectId, ref: "product", required: true },
   paymethod_id: { type: mongoose.Schema.Types.ObjectId, ref: "paymethod", required: true },
--- src/v1/routes/sale.js
+++ src/v1/routes/sale.js
@@ -3,8 +3,112 @@ import * as Sales from "../../controllers/sale.js";
 
 const router = express.Router();
 
-router.post("/create", Sales.createSale);
+/**
+ * @openapi
+ * tags:
+ *   name: Sale
+ */
+
+/**
+ * @openapi
+ * /api/v1/sales:
+ *   get:
+ *     summary: Get all sales
+ *     tags: [Sale]
+ *     parameters:
+ *       - in: query
+ *         name: p
+ *         schema:
+ *           type: integer
+ *         default: 1
+ *         description: Page
+ *       - in: query
+ *         name: limit
+ *         schema:
+ *           type: integer
+ *         default: 10
+ *         description: Limit
+ *     responses:
+ *       200:
+ *         description: OK
+ *         content:
+ *          application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               sales:
+ *                 type: array
+ *                 items:
+ *                   $ref: '#/components/schemas/Sale'
+ *                 page:
+ *                   type: integer
+ *                 totalPages:
+ *                   type: integer
+ *       500:
+ *         description: FAILED
+ *         content:
+ *          application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               message:
+ *                 type: string
+ * /api/v1/sales/{id}:
+ *   get:
+ *     summary: Get one sale
+ *     tags: [Sale]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *     responses:
+ *       200:
+ *         description: OK
+ *         content:
+ *          application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Sale'
+ *       500:
+ *         description: FAILED
+ *         content:
+ *          application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               message:
+ *                 type: string
+ * /api/v1/sales/create:
+ *   post:
+ *     summary: Create a sale
+ *     tags: [Sale]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Sale'
+ *     responses:
+ *       201:
+ *         description: CREATED
+ *         content:
+ *          application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Sale'
+ *       500:
+ *         description: FAILED
+ *         content:
+ *          application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               message:
+ *                 type: string
+ */
+
 router.get("/", Sales.getSales);
 router.get("/:id", Sales.getSaleById);
+router.post("/create", Sales.createSale);
 
 export default router;
